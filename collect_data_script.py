9bb39b04c2f49ffdaff7a9ac0087b7e19789f6cd
Sofiya Semenova <novasofiya@gmail.com>
2018-08-21 18:19:12 -0400

    update

--- collect_data_script.py
+++ collect_data_script.py
@@ -1,18 +1,51 @@
 # Really gross pls don't look
+import re
+
+archive_file = open("archive.txt", "r")
+jake_text = open("jake.txt", "w")
+amir_text = open("amir.txt", "w")
+action_text = open("action.txt", "w")
+modifiers = open("modifiers.txt", "w")
 
 def get_text_from_line(line):
     line = line.strip()
     repls = ('<br>',''),('<span class="highlight">', ''),('</span>', ''),("INTRO", '')
     return reduce(lambda a, kv: a.replace(*kv), repls, line)
 
+def remove_actions_from_dialogue(line):
+    # import pdb; pdb.set_trace()
+    action = new_line = ""
+    print(line)
+    split_bracket = re.split(line, "(\[|\])")
+    save_next_section = False
+    for section in split_bracket:
+        if "[" in section:
+            save_next_section = True
+        elif save_next_section:
+            action += section
+        elif "]" in section:
+            save_next_section = False
+        else:
+            new_line += section
+
+    split_paren = re.split(new_line, "(\(|\))")
+    new_line = ""
+    for section in split_paren:
+        if "(" in section:
+            save_next_section = True
+        elif save_next_section:
+            modifiers.write(section + "\n")
+            print(section + "\n")
+        elif ")" in section:
+            save_next_section = False
+        else:
+            new_line += section
+
+    return new_line, action
+
 def reset_for_next_video():
     return False, "", "", False
 
-archive_file = open("archive.txt", "r")
-jake_text = open("jake.txt", "w")
-amir_text = open("amir.txt", "w")
-action_text = open("action.txt", "w")
-# misc_text = open("misc.txt", "w")
 
 is_script, speaker_carryover, full_quote, force_newline = reset_for_next_video()
 for line in archive_file:
@@ -21,17 +54,21 @@ for line in archive_file:
             is_script, speaker_carryover, full_quote, force_newline = reset_for_next_video()
         elif line.strip() == "<br>" or force_newline:
             if speaker_carryover == "jake":
-                jake_text.write(full_quote + "\n")
+                full_quote, action = remove_actions_from_dialogue(full_quote)
+                if full_quote:
+                    jake_text.write(full_quote + "\n")
             elif speaker_carryover == "amir":
-                amir_text.write(full_quote + "\n")
+                full_quote, action = remove_actions_from_dialogue(full_quote)
+                if full_quote:
+                    amir_text.write(full_quote + "\n")
             elif speaker_carryover == "action":
                 if not full_quote == "[End]" and not "[]" in full_quote:
-                    action_text.write(full_quote + "\n")
+                    action_text.write(full_quote.strip("[]") + "\n")
             speaker_carryover = ""
             full_quote = ""
             force_newline = False
         elif speaker_carryover:
-            full_quote = full_quote + get_text_from_line(line) + " "
+            full_quote += get_text_from_line(line) + " "
         else:
             if "<br>" in line:
                 force_newline = True
@@ -43,9 +80,11 @@ for line in archive_file:
                     speaker_carryover = "jake"
                 elif "AMIR" in speaker or "Amir" in speaker:
                     speaker_carryover = "amir"
-                full_quote = full_quote + split_line[1].lstrip() + " "
+                print(line)
+                for section in split_line[1:]:
+                    full_quote += section.lstrip() + " "
             elif "[" in cleaned_line:
                 speaker_carryover = "action"
-                full_quote = full_quote + cleaned_line + " "
+                full_quote += cleaned_line + " "
     elif "<div class=\"episode-script-inner\">" in line:
         is_script = True
--- generate.py
+++ generate.py
@@ -1,35 +1,127 @@
 import numpy as np
 
-amir = open('amir.txt').read()
-jake = open('jake.txt').read()
+def setup():
+    amir_text = open('amir.txt').read()
+    jake_text = open('jake.txt').read()
+    action_text = open('action.txt').read()
 
-amir_corpus = amir.split()
-jake_corpus = jake.split()
+    amir_corpus = amir_text.split()
+    jake_corpus = jake_text.split()
+    action_corpus = action_text.split()
 
-def make_pairs(corpus):
-    for i in range(len(corpus)-1):
-        yield (corpus[i], corpus[i+1])
+    word_dict_amir = get_word_dict(amir_corpus)
+    word_dict_jake = get_word_dict(jake_corpus)
+    word_dict_actions = get_word_dict(action_corpus)
 
-pairs = make_pairs(corpus)
+    Amir = {"corpus": amir_corpus, "word_dict": word_dict_amir}
+    Jake = {"corpus": jake_corpus, "word_dict": word_dict_jake}
+    Actions = {"corpus": action_corpus, "word_dict": word_dict_actions}
 
-word_dict = {}
+    return Amir, Jake, Actions
 
-for word_1, word_2 in pairs:
-    if word_1 in word_dict.keys():
-        word_dict[word_1].append(word_2)
-    else:
-        word_dict[word_1] = [word_2]
+def get_word_dict(corpus):
+    def make_pairs(corpus):
+        for i in range(len(corpus)-1):
+            yield (corpus[i], corpus[i+1])
 
-first_word = np.random.choice(corpus)
+    pairs = make_pairs(corpus)
 
-while first_word.islower():
+    word_dict = {}
+
+    for word_1, word_2 in pairs:
+        if word_1 in word_dict.keys():
+            word_dict[word_1].append(word_2)
+        else:
+            word_dict[word_1] = [word_2]
+
+    return word_dict
+
+def generate_interleaving():
+    length_allowed = np.random.randint(2,6)
+    previous_character = np.random.randint(0,2) # 0-Jake, 1-Amir, 2-Actions
+    interleaving = []
+    for i in range(length_allowed):
+        if previous_character == 0:
+            next_character = np.random.randint(1,2)
+        elif previous_character == 1:
+            next_character = 2 if np.random.randint(0,1) == 1 else 0
+        else:
+            next_character = np.random.randint(0,2)
+        interleaving.append(next_character)
+        previous_character = next_character
+
+    return interleaving
+
+def generate_action(person_dict):
+    corpus = person_dict["corpus"]
+    word_dict = person_dict["word_dict"]
+    terminate_chars = ["]"]
+
     first_word = np.random.choice(corpus)
 
-chain = [first_word]
+    while not "[" in first_word:
+        first_word = np.random.choice(corpus)
 
-n_words = 50
+    chain = [first_word]
 
-for i in range(n_words):
-    chain.append(np.random.choice(word_dict[chain[-1]]))
+    n_words = np.floor(np.random.normal(10, 5))
 
-print(' '.join(chain))
+    generate = True
+    while generate:
+        new_word = np.random.choice(word_dict[chain[-1]])
+        while "[" in new_word:
+            new_word = np.random.choice(word_dict[chain[-1]])
+        if "]" in new_word:
+            generate = False
+        chain.append(new_word)
+
+    return ' '.join(chain)
+
+
+def generate_dialogue(person_dict):
+    corpus = person_dict["corpus"]
+    word_dict = person_dict["word_dict"]
+    terminate_chars = [".", "?", "???", ". . . ?", "...", "--", "!"]
+    open_punctuation = ["(", ")", "[", "]", "\""]
+
+    first_word = np.random.choice(corpus)
+
+    while first_word.islower():
+        first_word = np.random.choice(corpus)
+
+    chain = [first_word]
+
+    # n_words = np.floor(np.random.normal(15, 10))
+    n_words = np.random.randint(5, 20)
+    generate = True
+    count = 0
+    while generate:
+        new_word = np.random.choice(word_dict[chain[-1]])
+        while any(word in new_word for word in open_punctuation):
+            new_word = np.random.choice(word_dict[chain[-1]])
+
+        count += 1
+        if any(word in new_word for word in terminate_chars) and count > n_words:
+            generate = False
+
+        chain.append(new_word)
+
+
+    return ' '.join(chain)
+
+
+Amir, Jake, Actions = setup()
+interleaving = generate_interleaving()
+print(interleaving)
+
+print("AMIR")
+for i in range(0,10):
+    print(generate_dialogue(Amir))
+
+print("JAKE")
+for i in range(0,10):
+    print(generate_dialogue(Jake))
+
+print("ACTION")
+for i in range(0,10):
+    print(generate_action(Actions))
